7401e79ba81493551ad00326c06deb1b4b2ff42e
Sergey <sergeysirotenko@gmail.com>
2017-09-06 14:40:54 +0600

    commit

--- lib/functions.php
+++ lib/functions.php
@@ -1,9 +1,11 @@
 <?php
 
 /**
-*@author Sergey
+* Функция реализует подключение страниц
+*
+* @author Sergey
 */
-function print_content(){
+function print_content() {
 
     if ($_GET['page']) {
         include __DIR__ . '/../pages/' . $_GET['page'] . '.php';
